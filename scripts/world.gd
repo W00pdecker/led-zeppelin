19b5411934685f815a8ad19791eee7d8a32eb57e
W00pdecker <khatkhree@gmail.com>
2026-05-14 14:14:35 +0300

    Fixed bugs

--- scripts/world.gd
+++ scripts/world.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var levels = {
+	0: $Level0,
 	1: $Level1,
 	2: $Level2,
 	3: $Level3,
@@ -9,6 +10,8 @@ extends Node2D
 func _ready():
 	for level in levels.values():
 		_hide_level(level)
+	_show_level(levels[0])
+		
 
 	GameManager.passenger_collected.connect(_on_passenger_collected)
 
@@ -16,6 +19,7 @@ func _ready():
 func _on_passenger_collected(total: int):
 	if levels.has(total):
 		_show_level(levels[total])
+		_hide_level(levels[total-1])
 		print("Открылась новая платформа!")
 
 
@@ -25,6 +29,9 @@ func _hide_level(level: Node2D):
 	for child in level.get_children():
 		if child is TileMapLayer:
 			child.collision_enabled = false
+		if child is Area2D:
+			child.monitoring = false
+			child.monitorable = false
 
 
 func _show_level(level: Node2D):
@@ -33,3 +40,6 @@ func _show_level(level: Node2D):
 	for child in level.get_children():
 		if child is TileMapLayer:
 			child.collision_enabled = true
+		if child is Area2D:
+			child.monitoring = true
+			child.monitorable = true
